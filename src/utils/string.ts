0f510defa880fa7e52247990b040600df92ff4e0
webcreatis <contact@webcreatis.fr>
2024-12-08 10:33:27 +0400

    TSE-03 > Add string.ts

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,4 +1,4 @@
-export function convertStringToBoolean(input) {
+export function convertStringToBoolean(input: string | boolean): boolean {
   if (typeof input === "boolean") return input
   return input === "true"
 }
