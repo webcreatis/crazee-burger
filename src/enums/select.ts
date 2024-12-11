2b535ec57725ef51a473731dbef630e5b81962e4
webcreatis <contact@webcreatis.fr>
2024-12-11 10:30:04 +0400

    TSE-05-enums > Add select.ts

--- src/enums/select.ts
+++ src/enums/select.ts
@@ -1,9 +1,16 @@
-export const isAvailableOptions = [
+
+type Options = {
+  optionValue: boolean;
+  label: string;
+}[]
+
+
+export const isAvailableOptions:Options = [
   { optionValue: true, label: "En stock" },
   { optionValue: false, label: "En rupture" },
 ]
 
-export const isPublicisedOptions = [
+export const isPublicisedOptions:Options = [
   { optionValue: false, label: "Sans pub" },
   { optionValue: true, label: "Avec pub" },
 ]
