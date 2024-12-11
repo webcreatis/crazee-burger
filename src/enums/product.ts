cd832ea61de27c80a1c2272124f98a5a26866a95
webcreatis <contact@webcreatis.fr>
2024-12-11 10:43:04 +0400

    TSE-05-enums > Add product.ts

--- src/enums/product.ts
+++ src/enums/product.ts
@@ -1,4 +1,12 @@
-export const EMPTY_PRODUCT = Object.freeze({
+import { MenuProduct } from "@/types/Product"
+
+type BasketMessage = {
+  EMPTY: string,
+  LOADING: string,
+  NOT_AVAILABLE: string,
+}
+
+export const EMPTY_PRODUCT:MenuProduct = Object.freeze({
   id: "",
   title: "",
   imageSource: "",
@@ -10,7 +18,7 @@ export const EMPTY_PRODUCT = Object.freeze({
 export const IMAGE_COMING_SOON = "/images/coming-soon.png"
 export const IMAGE_NO_STOCK = "/images/stock-epuise.png"
 
-export const BASKET_MESSAGE = {
+export const BASKET_MESSAGE:BasketMessage = {
   EMPTY: "Votre commande est vide.",
   LOADING: "Chargement en cours...",
   NOT_AVAILABLE: "Non disponible",
